7ca400064e349b7044277d5458a7f1c9961c51f1
Loyiso04 <loyisokhumalo04@gmail.com>
2024-06-17 18:17:27 +0200

    [Feature]: Added additional routes to the app

--- api/index.js
+++ api/index.js
@@ -1,13 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 8080;
 
-app.get('/',(req,res) =>{
-    res.send('Hello World!')
-})
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
 
-app.listen(port, () =>{
-    console.log(`Server running on port ${port}`)
-})
+app.get('/user', (req, res) => {
+    const users = [
+        {
+            id: "1",
+            name: "User1",
+            email: 'user1@gmail.com',
+        },
+        {
+            id: "2",
+            name: "User2",
+            email: "user2@gmail.com",
+        },
+        {
+            id: "3",
+            name: "User3",
+            email: "user3@gmail.com"
+        },
+    ];
+    res.json(users).status(200);
+});
+
+app.post('/users', (req, res) => {
+    const data = req.body;
+    console.log(data); 
+    res.json("success").status(200)
+
+});
+
+app.delete("/users/:id")
+
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
+});
 
 module.exports = app;
+
